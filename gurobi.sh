0fc212dceef3027b089b3917f2183908d7243bfe
hellman <hellman@users.noreply.github.com>
2021-04-14 17:34:35 +0200

    save commands

--- gurobi.sh
+++ gurobi.sh
@@ -6,4 +6,6 @@ read qwe
 
 rm -f "$1"_* "$1".*
 
+# gurobi_cl "MIPFocus=3" "ResultFile=$1.sol" "SolFiles=$1" "LogFile=$1.log" "$1"
+#gurobi_cl "ObjBound=1899" "ResultFile=$1.sol" "SolFiles=$1" "LogFile=$1.log" "$1"
 gurobi_cl "ResultFile=$1.sol" "SolFiles=$1" "LogFile=$1.log" "$1"
